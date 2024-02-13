5fd1b39d77160b34dc5ac01b3f2d33ecd89b86a5
Alana Enikeeva <120022626+Allnnel@users.noreply.github.com>
2024-02-13 13:34:32 +0300

    Update s21_other.c

--- src/s21_other.c
+++ src/s21_other.c
@@ -95,33 +95,31 @@ int s21_round(s21_decimal value, s21_decimal *result) {
 
 int s21_bankround(s21_decimal value, s21_decimal *result) {
   int res = s21_Decimal_ToNull(result);
-  //    result->bits[0] = result->bits[1] = result->bits[2] = result->bits[3] =
-  //    0;//зануляем выходной массив//успех
-  int sign = s21_GetSign(value);  //записываем знак
-  s21_SetSign(&value, 0);  //убираем минус для расчётов если он есть
+  int sign = s21_GetSign(value); 
+  s21_SetSign(&value, 0);
   s21_decimal rem = {{0}};
   s21_decimal res2 = {{0}};
-  s21_decimal int_val = {{0, 0, 0, 0}};  //массив для целой части числа
-  s21_decimal fract_val = {{0, 0, 0, 0}};  //массив для дробной части числа
-  s21_decimal one = {{0b00000000000000000000000000000001, 0, 0, 0}};   // 1
-  s21_decimal null = {{0b00000000000000000000000000000000, 0, 0, 0}};  // 0
-  if (result) {  //проверка на null
-    s21_truncate(value, &int_val);  //записываем целую часть с помощью trunс
+  s21_decimal int_val = {{0, 0, 0, 0}};  
+  s21_decimal fract_val = {{0, 0, 0, 0}}; 
+  s21_decimal one = {{0b00000000000000000000000000000001, 0, 0, 0}};  
+  s21_decimal null = {{0b00000000000000000000000000000000, 0, 0, 0}}; 
+  if (result) { 
+    s21_truncate(value, &int_val); 
     s21_sub(value, int_val,
-            &fract_val);  //записываем дробную вычитая целую из числа
+            &fract_val); 
     div_other(int_val, &res2, &rem, 2);
     if (s21_is_equal(rem, null)) {
       *result = int_val;
-      s21_SetScale(result, 0);  //выставляем экспоненту
-      s21_SetSign(result, sign);  //выставляем изначальный знак
+      s21_SetScale(result, 0); 
+      s21_SetSign(result, sign); 
     } else {
-      s21_add(int_val, one, result);  //округляем прибавляя единицу к целому
-      s21_SetScale(result, 0);  //выставляем экспоненту
-      s21_SetSign(result, sign);  //выставляем изначальный знак
+      s21_add(int_val, one, result); 
+      s21_SetScale(result, 0); 
+      s21_SetSign(result, sign); 
     }
 
   } else {
-    res = 1;  //ошибка
+    res = 1; 
   }
   return res;
 }
